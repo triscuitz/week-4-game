8494463d6e38fc1655a494b55b89223820cffe0e
Nicholas Moore <nmoore@Nicholass-MacBook-Pro.local>
2017-12-02 09:53:18 -0700

    Added attck

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,48 +4,53 @@ var characters = {
   'luke': {
       name: 'Luke Skywalker',
       health: 150,
-      attackPow: 10,
-      counterAtk: 5
+      attackPow: 8,
+      counterAtk: 20
   },
   'boba': {
       name: 'Boba Fett',
       health: 180,
-      attackPow: 10,
-      counterAtk: 5
+      attackPow: 7,
+      counterAtk: 20
   },
   'rey': {
       name: 'Rey',
       health: 100,
-      attackPow: 10,
+      attackPow: 14,
       counterAtk: 5
   },
   'kylo': {
       name: 'Kylo Ren',
       health: 120,
-      attackPow: 10,
-      counterAtk: 5
+      attackPow: 8,
+      counterAtk: 15
   }
 };
 
 var attacker;
 var atkHealth;
 var attackPow;
+var basePow;
 var defender;
 var defHealth;
 var counterAtk;
+var selectedDefender;
 
 function fight() {
   attacker = false;
   atkHealth ='';
   attackPow = '';
+  basePow = '',
   defender = false;
-  defHealth ='',
+  defHealth ='';
   counterAtk = '';
 
   $('').empty();
 };
 
-<!-- Attachers ON/OFF -->
+// Attachers ON/OFF
+
+// if your warrior is luke
 $('#lukeWar').on('click', function() {
   $('#bobaWar').hide();
   $('#reyWar').hide();
@@ -55,7 +60,10 @@ $('#lukeWar').on('click', function() {
   attacker = true;
   atkHealth = characters.luke.health;
   attackPow = characters.luke.attackPow;
+  basePow = characters.luke.attackPow;
 });
+
+// if your warrior is boba fett
 $('#bobaWar').on('click', function() {
   $('#lukeWar').hide();
   $('#reyWar').hide();
@@ -65,7 +73,10 @@ $('#bobaWar').on('click', function() {
   attacker = true;
   atkHealth = characters.boba.health;
   attackPow = characters.boba.attackPow;
+  basePow = characters.boba.attackPow;
 });
+
+// if your warrior is rey
 $('#reyWar').on('click', function() {
   $('#bobaWar').hide();
   $('#lukeWar').hide();
@@ -75,7 +86,10 @@ $('#reyWar').on('click', function() {
   attacker = true;
   atkHealth = characters.rey.health;
   attackPow = characters.rey.attackPow;
+  basePow = characters.rey.attackPow;
 });
+
+// if your warrior is kylo
 $('#kyloWar').on('click', function() {
   $('#bobaWar').hide();
   $('#reyWar').hide();
@@ -84,60 +98,94 @@ $('#kyloWar').on('click', function() {
   $('#kyloOther').hide();
   attacker = true;
   atkHealth = characters.kylo.health;
-  attackPow = characters.kylo.counterAtk;
+  attackPow = characters.kylo.attackPow;
+  basePow = characters.kylo.attackPow;
 });
 
 
-<!-- Challangers ON/OFF -->
+// Challangers ON/OFF
+
+// if your challanger is luke
 $('#lukeOther').on('click', function() {
   $('#lukeOther').hide();
   $('#lukeChal').show();
   defender = true;
+  selectedDefender = $('#LukeChal');
   defHealth = characters.luke.health;
   counterAtk = characters.luke.counterAtk;
 });
+
+// if your challanger is boba
 $('#bobaOther').on('click', function() {
   $('#bobaOther').hide();
   $('#bobaChal').show();
   defender = true;
+  selectedDefender = $('#bobaChal');
   defHealth = characters.boba.health;
   counterAtk = characters.boba.counterAtk;
 });
+
+// if your challanger is rey
 $('#reyOther').on('click', function() {
   $('#reyOther').hide();
   $('#reyChal').show();
   defender = true;
+  selectedDefender = $('#reyChal')
   defHealth = characters.rey.health;
   counterAtk = characters.rey.counterAtk;
 });
+
+// if your challanger is kylo
 $('#kyloOther').on('click', function() {
   $('#kyloOther').hide();
   $('#kyloChal').show();
   defender = true;
+  selectedDefender = $('#kyloChal');
   defHealth = characters.kylo.health;
   counterAtk = characters.kylo.counterAtk;
 });
+
+//attack button mechanics
 $('#attack').on('click', function(){
-  if (defender) {
-    console.log(defHealth);
+  if (defender && attacker && defHealth > 0 && atkHealth > 0) {
     defHealth = defHealth - attackPow;
-    console.log(defHealth);
-    console.log(attackPow);
-    attackPow = attackPow + attackPow;
-    console.log(attackPow);
     atkHealth = atkHealth - counterAtk;
-    console.log(atkHealth);
+
+//displaying the results of the attack
+    $('#resultsArea').html(
+      'You attacked for: ' + attackPow +
+      ' You were countered for: ' + counterAtk
+      );
+      attackPow = attackPow + basePow;
+    };
+
     if (defHealth <= 0) {
       defender = false;
-      console.log('you won');
-    };
+      selectedDefender.hide();
+      $('#resultsArea').html('You Defeted the enemy! Chose a new opponent!');
+      defHealth = '';
+      };
+
     if (atkHealth <= 0) {
-      console.log('you lose');
-    }
-  };
+      attacker = false;
+      $('#resultsArea').html('You lose! Click retreat to try again!');
+      };
+
+      //getting the warriors health to change
+      $('#lukeWarHealth').html('Health: ' + atkHealth);
+      $('#bobaWarHealth').html('Health: ' + atkHealth);
+      $('#reyWarHealth').html('Health: ' + atkHealth);
+      $('#kyloWarHealth').html('Health: ' + atkHealth);
+
+      //getting the challangers health to change
+      $('#lukeChalHealth').html('Health: ' + defHealth);
+      $('#bobaChalHealth').html('Health: ' + defHealth);
+      $('#reyChalHealth').html('Health: ' + defHealth);
+      $('#kyloChalHealth').html('Health: ' + defHealth);
 
 });
 
+// retreat button mechanics aka reset
 $('#retreat').on('click', function(){
   location.reload();
 });
